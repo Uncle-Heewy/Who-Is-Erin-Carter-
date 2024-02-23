9ed2c6d6de6b72c56a3335d0bfbe5d7d4a9430a4
Uncle-Heewy <161084882+Uncle-Heewy@users.noreply.github.com>
2024-02-24 00:45:52 +0300

    Add files via upload
    Adding index.html, styles.css, script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+
+  var recommendBtn = document.getElementById("recommend_btn");
+
+    // Adding an onclick event to invoke the addRecommendation function
+    recommendBtn.onclick = function() {
+        addRecommendation();
+    };
+
+  showPopup(true);
+
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
